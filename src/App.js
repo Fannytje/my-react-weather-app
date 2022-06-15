9f0a3bff4fcea1c6c02c8b65cde312499ea64e25
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-15 15:25:12 +0200

    Added the link to Github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1> 
+      <footer>
+      This project was coded by Fanny and is <a href="https//:github.com/Fannytje/my-react-weather-app" target="_blank">open-sourced on Github.</a>
+      </footer>
     </div>
   );
 }
