6c49ea5249b6d87aa70cbe0f7917e7773053c07a
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-15 17:40:59 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1> 
-      <footer>
-      This project was coded by Fanny and is <a href="https//:github.com/Fannytje/my-react-weather-app" target="_blank">open-sourced on Github.</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1> 
+        <Weather />
+        <footer>
+        This project was coded by Fanny and is <a href="https//:github.com/Fannytje/my-react-weather-app" target="_blank">open-sourced on Github.</a>
+        </footer>
+      </div>
     </div>
   );
-}
+} 
 
 
