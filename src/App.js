19cfb1bdfd8a060ca5e6d7b84a248b78f8de1612
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-19 11:02:38 +0200

    Removed the title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1> 
         <Weather defaultCity="Paris"/>
         <footer>
         This project was coded by Fanny and is <a href="https//:github.com/Fannytje/my-react-weather-app" target="_blank">open-sourced on Github.</a>
