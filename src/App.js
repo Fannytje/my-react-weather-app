d990510ba3fb4ea295567650b69f88ae6de56c79
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-24 12:08:42 +0200

    Added daily forecast for 5 days and fixed small issues

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Paris"/>
+        <Weather defaultCity="Amsterdam"/>
         <footer>
-        This project was coded by Fanny and is <a href="https//:github.com/Fannytje/my-react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a> and{" "}
+        This project was coded by Fanny Nordberg and is{" "} 
+          <a 
+            href="https//:github.com/Fannytje/my-react-weather-app" 
+            target="_blank" 
+            rel="noreferrer noopener">open-sourced on Github</a> and{" "}
           <a
             href="https://bucolic-sfogliatella-655015.netlify.app/"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer noopener"
           >
             hosted on Netlify.
           </a>
